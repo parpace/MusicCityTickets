9d9b2284adbcbee4f43368d9c4a97b1c95094a2b
parpace <parpace@gmail.com>
2024-07-15 13:55:58 -0500

    React app created

---
+++ front-end/musicCityTickets/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Header from './components/Header'
+import Body from './components/Body'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  )
+}
+
+export default App
